9690d853705f09bb726e3f8bf5883930fe5ffa95
knathanieltucker <k.nathaniel.tucker@gmail.com>
2016-01-21 08:26:08 -0800

    changed view poll
    view poll is now more modular. It relies on barChart

--- client/templates/authenticated/viewPoll/viewPoll.js
+++ client/templates/authenticated/viewPoll/viewPoll.js
@@ -2,48 +2,10 @@ Template.viewPoll.onCreated(function() {
 
   Template.instance().subscribe( 'singlePoll',
     FlowRouter.getParam('pollId') );
-  Template.instance().subscribe( 'singlePollStances',
-    FlowRouter.getParam('pollId') );
 
 });
 
 Template.viewPoll.onRendered(function () {
-
-  var dataMapping = {}, index = 0, loadingData = true;
-
-  var ctx = $("#pollBarChart").get(0).getContext("2d");
-  var pollBarChart = new Chart(ctx).Bar({
-    labels: ["Loading!"],
-    datasets: [{
-      fillColor: "rgba(220,220,220,0.5)",
-      strokeColor: "rgba(220,220,220,0.8)",
-      highlightFill: "rgba(220,220,220,0.75)",
-      highlightStroke: "rgba(220,220,220,1)",
-      data: [1]
-    }]
-  }, {});
-
-
-  Template.instance().pollHandle = Stances.find({
-    pollId: FlowRouter.getParam('pollId')
-  }).observeChanges({
-    added: function (id, stance) {
-      if (dataMapping[id] !== undefined)
-        return;
-
-      dataMapping[id] = index++;
-      pollBarChart.addData([stance.votes.count], stance.stance);
-
-      if (loadingData) {
-        loadingData = false;
-        pollBarChart.removeData();
-      }
-    },
-    changed: function (id, stance) {
-      pollBarChart.datasets[0].bars[dataMapping[id]].value = stance.votes.count;
-      pollBarChart.update();
-    }
-  });
 
 });
 
@@ -52,12 +14,7 @@ Template.viewPoll.onDestroyed(function () {
 });
 
 Template.viewPoll.helpers({
-  pollTitle: function () {
-    var poll = Polls.findOne();
-    return poll && poll.title;
-  },
-  pollSubtitle: function () {
-    var poll = Polls.findOne();
-    return poll && poll.subtitle;
+  poll: function () {
+    return Polls.findOne();
   }
 });
